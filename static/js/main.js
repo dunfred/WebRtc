f8dbf5776ba251149fab57f44f9b3a4de3cc41ad
dunfred <donklenam2@gmail.com>
2022-06-29 23:37:46 +0000

    initial files

---
+++ static/js/main.js
@@ -0,0 +1,55 @@
+let peerConnection;
+let localStream;
+let remoteStream;
+
+let servers = {
+    iceServers:[
+        {
+            urls: [
+                'stun:stun1.1.google.com:19302',
+                'stun:stun2.1.google.com:19302'
+            ]
+        }
+    ]
+}
+
+let init = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
+        
+    document.getElementById('user-1').srcObject = localStream;
+}
+
+let createOffer = async () => {
+    peerConnection = new RTCPeerConnection(servers)
+
+    remoteStream = new MediaStream()
+    document.getElementById('user-2').srcObject = remoteStream;
+
+    document.getElementById('answer-sdp').innerHTML = localStream;
+    console.log(localStream);
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    })
+
+    peerConnection.ontrack = async (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track)
+        })
+    }
+
+    peerConnection.onicecandidate = async (event) => {
+        if (event.candidate){
+            document.getElementById('offer-sdp').value = JSON.stringify(peerConnection.localDescription)
+        }
+    }
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    document.getElementById('offer-sdp').value = JSON.stringify(offer)
+}
+
+
+init()
+
+document.getElementById('create-offer').addEventListener('click', createOffer);
